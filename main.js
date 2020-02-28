014bcf55664df8dd23f8484eadc3d3fce1ed3502
Daniel Burchett <60920195+Dnlburch@users.noreply.github.com>
2020-02-27 23:25:09 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+//Smooth scroll
+var scroll = new SmoothScroll('a[href*="#projects"]',{
+  speed: 800,
+  offset: 100
+});
+
+var topPage = new SmoothScroll('a[href*="#top"]',{
+  speed: 800,
+  offset: 500
+});
